ba28f2d619f7d930eb37f1a133ef46d793a105d1
Jose Maria Infanzon <jocheinfa@gmail.com>
2020-04-28 08:41:18 -0300

    New Commit
    Signed-off-by: Jose Maria Infanzon <jocheinfa@gmail.com>

--- app/src/main/java/io/iic/helloworld/Calculator.java
+++ app/src/main/java/io/iic/helloworld/Calculator.java
@@ -27,7 +27,7 @@ public class Calculator implements CalculatorService {
 
     @Override
     public double multiply(double input1, double input2) {
-        Log.d(Calculator.class.getCanonicalName(), "This is multiply");
+        Log.d(Calculator.class.getCanonicalName(), "This is multiply operation");
         return 0;
     }
 
